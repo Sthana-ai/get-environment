939718cb8b30bf993e1c9d63846ee18d920622d0
Danny Paul <dannypaulg@gmail.com>
2021-05-13 21:03:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const action = async () => {
+  try {
+    const environment = core.getInput('environment', { required: true });
+    const ciToken = core.getInput('ci-token', { required: true });
+    const serviceName = core.getInput('service-name', { required: true });
+
+    const octokit = github.getOctokit(ciToken);
+
+    const serviceStatesRes = await octokit.repos.getContent({
+      owner: 'Sthana-ai',
+      repo: 'environments',
+      path: `services/${environment}.json`,
+      ref: 'refs/heads/master',
+    });
+    const serviceStates = JSON.parse(Buffer.from(serviceStatesRes.data.content, 'base64').toString());
+    const serviceState = serviceStates.find((state) => state.name === serviceName);
+    if (!serviceState) {
+      core.setFailed(`service state not found for ${serviceName}`);
+    }
+  } catch (error) {
+    core.setFailed(error);
+  }
+};
+
+action();
